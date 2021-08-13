d67ea2a3bf46feecfacefb3e44e795c6575b85d4
sam khe <visuelkoncept@gmail.com>
2021-08-13 16:22:44 +0200

    formulaire

--- panier_script.js
+++ panier_script.js
@@ -80,16 +80,21 @@ let btn_envoyerleformulaire = document.querySelector('.bouton_commande');
 //adEvenetlistener
 
 btn_envoyerleformulaire.addEventListener('click',function(e){
-    e.preventDefault;
+    e.preventDefault();
 
-    // mettre les valeurs du formulaire dans un objet
 validateName();
+validateSurname();
+validerEmail();
+validateAdress();
+validateCP();
+validateCity();
+
+
     const leformulaire = {
 
         nom : document.querySelector('#votrenom').value,
         prenom : document.querySelector('#prenom').value,
         email : document.querySelector('#email').value,
-        date : document.querySelector('#date').value,
         adresse : document.querySelector('#adresse').value,
         code_postal : document.querySelector('#cp').value,
         ville : document.querySelector('#ville').value
@@ -102,7 +107,7 @@ validateName();
 function validateName(){
     let inputName = document.querySelector('#votrenom');
     let valueName = inputName.value;
-    let errorName = document.querySelector('.error');
+    let errorName = document.querySelector('.nom');
     //trim permet de supprimer les espaces au extremités d'une chaine de caractères
     if ( !valueName.trim()){
         errorName.innerText = 'Veuillez remplir ce champ';
@@ -120,209 +125,106 @@ function validateName(){
     return true;
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*</div>let container = document.querySelector('.container');
-
-
-// BLOC NOM ET PRENOM 
-
-// CASE NOM
-
-let section1 = document.createElement('div');
-section1.classList.add('section1');
-container.appendChild(section1);
-
-let coordonnees = document.createElement('h3');
-coordonnees.classList.add('h3');
-coordonnees.innerText = 'Vos coordonnées';
-section1.appendChild(coordonnees);
-
-let form = document.createElement('form');
-form.classList.add('form');
-section1.appendChild(form);
-
-let nom_et_prenom = document.createElement('div');
-nom_et_prenom.classList.add('nom_et_prenom');
-form.appendChild(nom_et_prenom);
-
-let div_name = document.createElement('div');
-div_name.classList.add('div_name');
-nom_et_prenom.appendChild(div_name);
-
-let name_label = document.createElement('label');
-name_label.classList.add('name_label');
-name_label.innerText = 'Nom';
-nom_et_prenom.appendChild(name_label);
-
-let name_input = document.createElement('input');
-name_input.classList.add('name_input')
-name_input.type = 'text';
-nom_et_prenom.appendChild(name_input);
-
-//CASE PRENOM
-
-let div_prenom = document.createElement('div');
-div_prenom.classList.add('div_prenom');
-nom_et_prenom.appendChild(div_prenom);
-
-let prenom_label = document.createElement('label');
-prenom_label.classList.add('prenom_label');
-prenom_label.innerText = 'Prénom';
-nom_et_prenom.appendChild(prenom_label);
-
-let prenom_input = document.createElement('input');
-prenom_input.classList.add('prenom_input')
-prenom_input.type = 'text';
-nom_et_prenom.appendChild(prenom_input);
-
-// CASE EMAIL
-
-
-let div_email = document.createElement('div');
-div_email.classList.add('div_email');
-form.appendChild(div_email);
-
-let email_label = document.createElement('label');
-email_label.classList.add('email_label');
-email_label.innerText = 'Email';
-div_email.appendChild(email_label);
-
-let email_input = document.createElement('input');
-email_input.classList.add('email_input')
-email_input.type = 'email';
-div_email.appendChild(email_input);
-
-
-// CASE DATE DE NAISSANCE
-
-let date_de_naissance = document.createElement('div');
-container.appendChild(date_de_naissance);
-
-let div_date_de_naissance = document.createElement('div');
-div_date_de_naissance.classList.add('div_date_de_naissance');
-form.appendChild(div_date_de_naissance);
-
-let date_de_naissance_label = document.createElement('label');
-date_de_naissance_label.classList.add('date_de_naissance_label');
-date_de_naissance_label.innerText = 'Date de naissance';
-div_date_de_naissance.appendChild(date_de_naissance_label);
-
-let date_de_naissance_input = document.createElement('input');
-date_de_naissance_input.classList.add('date_de_naissance_input')
-date_de_naissance_input.type = 'date';
-div_date_de_naissance.appendChild(date_de_naissance_input);
-
-// CASE ADRESSE
-
-let adresse = document.createElement('div');
-container.appendChild(adresse);
-
-let div_adresse = document.createElement('div');
-div_adresse.classList.add('div_adresse');
-form.appendChild(div_adresse);
-
-let adresse_label = document.createElement('label');
-adresse_label.classList.add('adresse_label');
-adresse_label.innerText = 'Adresse';
-div_adresse.appendChild(adresse_label);
-
-let adresse_input = document.createElement('input');
-adresse_input.classList.add('adresse_input')
-adresse_input.type = 'text';
-div_adresse.appendChild(adresse_input);
-
-
-//BLOC CODE POSTAL ET VILLE
-
-//CASE CODE POSTAL
-
-let cp_ville = document.createElement('div');
-form.appendChild(cp_ville);
-
-let div_cp = document.createElement('div');
-div_cp.classList.add('div_cp');
-cp_ville.appendChild(div_cp);
-
-let cp_label = document.createElement('label');
-cp_label.classList.add('cp_label');
-cp_label.innerText = 'Code Postal';
-div_cp.appendChild(cp_label);
-
-let cp_input = document.createElement('input');
-cp_input.classList.add('cp_input')
-cp_input.type = 'number';
-div_cp.appendChild(cp_input);
-
-// CASE VILLE
-
-let div_ville = document.createElement('div');
-div_ville.classList.add('div_ville');
-cp_ville.appendChild(div_ville);
-
-let ville_label = document.createElement('label');
-ville_label.classList.add('cp_label');
-ville_label.innerText = 'Ville';
-div_ville.appendChild(ville_label);
-
-let ville_input = document.createElement('input');
-ville_input.classList.add('ville_input');
-ville_input.type = 'text';
-div_ville.appendChild(ville_input);
-
-
-//SECTION 2
-
-let section2 = document.createElement('div');
-section2.classList.add('section2');
-container.appendChild(section2);
-
-let recap = document.createElement('h3');
-recap.classList.add('h3');
-recap.innerText = 'Récapitulatif de votre commande';
-section2.appendChild(recap);
-
-let bouton_commande = document.createElement('button');
-bouton_commande.classList.add('bouton_commande');
-bouton_commande.innerText = 'Valider la commande et Payer';
-section2.appendChild(bouton_commande);
-
-
-
-console.log(container);*/
+function validateSurname(){
+    let inputSurname = document.querySelector('#prenom');
+    let valueSurname = inputSurname.value;
+    let errorSurname = document.querySelector('.prenom');
+    //trim permet de supprimer les espaces au extremités d'une chaine de caractères
+    if ( !valueSurname.trim()){
+        errorSurname.innerText = 'Veuillez remplir ce champ';
+        errorSurname.style.display = 'block';
+        inputSurname.value = '';// '' permet de vider le champs entierement
+        return false;
+    }
+    if (valueSurname.length < 2)
+    {
+        errorSurname.innerText = 'Votre prenom est trop court';
+        errorSurname.style.display = 'block';
+        return false;
+    }
+   
+
+}
+
+function validerEmail(){
+
+    let inputMail = document.getElementById('email');
+    let valueMail = inputMail.value;
+    let errorMail = document.querySelector('.email');
+    var regx = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/
+
+        if(!regx.test(inputMail.value)){
+            errorMail.innerText = 'Adresse email incorrecte';
+            errorMail.style.display = 'block';
+            inputMail.value = '';// '' permet de vider le champs entierement
+            inputMail.focus;
+            return false;
+        }
+        else{
+            errorMail.style.display = 'none';
+        }
+}
+
+
+function validateAdress(){
+    let inputAdress = document.querySelector('#adresse');
+    let valueAdress = inputAdress.value;
+    let errorAdress = document.querySelector('.adresse');
+    //trim permet de supprimer les espaces au extremités d'une chaine de caractères
+    if ( !valueAdress.trim()){
+        errorAdress.innerText = 'Veuillez remplir ce champ';
+        errorAdress.style.display = 'block';
+        inputAdress.value = '';// '' permet de vider le champs entierement
+        return false;
+    }
+    if (valueAdress.length < 5)
+    {
+        errorAdress.innerText = 'Adresse erronée';
+        errorAdress.style.display = 'block';
+        return false;
+    }
+    errorAdress.style.display = 'none';
+    return true;
+}
+
+function validateCP(){
+    let inputCP = document.querySelector('#cp');
+    let valueCP = inputCP.value;
+    let errorCP = document.querySelector('.cp');
+    //trim permet de supprimer les espaces au extremités d'une chaine de caractères
+    if ( !valueCP.trim()){
+        errorCP.innerText = 'Veuillez remplir ce champ';
+        errorCP.style.display = 'block';
+        inputCP.value = '';// '' permet de vider le champs entierement
+        return false;
+    }
+    if (valueCP.length < 5)
+    {
+        errorCP.innerText = 'Entrer un code postal correct';
+        errorCP.style.display = 'block';
+        return false;
+    }
+    errorCP.style.display = 'none';
+    return true;
+}
+
+function validateCity(){
+    let inputCity = document.querySelector('#ville');
+    let valueCity = inputCity.value;
+    let errorCity = document.querySelector('.ville');
+    //trim permet de supprimer les espaces au extremités d'une chaine de caractères
+    if ( !valueCity.trim()){
+        errorCity.innerText = 'Veuillez remplir ce champ';
+        errorCity.style.display = 'block';
+        inputCity.value = '';// '' permet de vider le champs entierement
+        return false;
+    }
+    if (valueCity.length < 2)
+    {
+        errorCity.innerText = 'Veuillez recommencer';
+        errorCity.style.display = 'block';
+        return false;
+    }
+    errorCity.style.display = 'none';
+    return true;
+}
