dd3633cd10d83ddca44d8f2a4d417387a38782c7
sam khe <visuelkoncept@gmail.com>
2021-08-15 16:40:49 +0200

    panier

--- panier_script.js
+++ panier_script.js
@@ -1,3 +1,262 @@
+let produitLocalStorage = JSON.parse(localStorage.getItem("produits"));
+console.log(produitLocalStorage);
+produits = produitLocalStorage;
+
+
+
+//-------------------------------------------------CALCUL SOUS TOTAL ---------------------------------------
+let sousTotalCalcul = [];
+for (m = 0; m < produits.length; m++) {
+  let quantité = produitLocalStorage[m].quantity;
+  let prixUnitaire = produitLocalStorage[m].price;
+
+  sousTotal = quantité * prixUnitaire;
+  sousTotalCalcul.push(sousTotal);
+  console.log("le soustotal est" + sousTotal);
+}//---------------------------------------------------FIN CALCUL SOUS TOTAL-------------------------------------------
+
+
+
+
+
+
+//----------------------------------------------------AFFICHAGE DES PRODUITS + PANIER VIDE-----------------------------
+
+
+//selection de la classe ou je vais injecter mon html
+const positionsElement = document.querySelector(".tbody");
+
+//si le panier est vide : afficher le panier est vide
+let structureProduitPanier = [];
+if (produits === null) {
+  const paniervide = `
+    <div class = "containerPanierVide">
+        <div>LE PANIER EST VIDE</div>
+        <a href="index.html">Retourner à la boutique</a>
+    </div>
+    `;
+  let tableau = document.querySelector(".tableau");
+  tableau.innerHTML = paniervide;
+} else {
+  // si le panier n'est pas vide , afficher les produits dans le localstorage
+  let nbproduit = 0;
+
+  for (j = 0; j < produits.length; j++) {
+    nbproduit += produits[j].quantity;
+    structureProduitPanier = structureProduitPanier + `
+       
+            <tr>
+                <td class="divNomCouleur">${produitLocalStorage[j].name}</td>
+                <td class="coloris">${produitLocalStorage[j].selectedColor}</td>
+                <td class="quantity">${produitLocalStorage[j].quantity}</td>
+                <td class="prix">${produitLocalStorage[j].price}</td>
+                <td class="soustotal">${produitLocalStorage[j].sousTotal}</td>
+
+            </tr>`;
+  }
+
+  if (j === produitLocalStorage.length);
+  {
+    positionsElement.innerHTML = structureProduitPanier;
+  }
+  console.log('il y a  ' + nbproduit + ' produits dans le panier');
+}
+
+//-------------------------------------------------TOTAL ---------------------------------------
+
+let prixTotalCalcul = [];
+const total = sousTotalCalcul.reduce((acc, cur) => acc + cur);
+
+console.log("le total est " + total);
+
+// le html du prix total :
+const afficherPrixTotal = `<div class="prixTotal">Le prix total est : ${total}</div>`;
+//selection de la classe ou je vais injecter mon html
+const positionsElement2 = document.querySelector(".tfoot");
+positionsElement2.innerHTML = afficherPrixTotal;
+
+//-----------------------------------VIDER LE PANIER-------------------------------------------------
+
+let btn_viderPanier = document.querySelector(".paniervide");
+
+//suppression de la key produit du localstorage
+
+btn_viderPanier.addEventListener("click", (e) => {
+  e.preventDefault();
+  localStorage.removeItem("produits");
+  alert("Le panier a été vidé");
+  window.location.href = "panier.html";
+});
+
+//-----------------------------------------FORMULAIRE-------------------------------------------------
+
+//selection du bouton
+
+let btn_envoyerleformulaire = document.querySelector(".bouton_commande");
+
+//adEvenetlistener
+
+btn_envoyerleformulaire.addEventListener("click", function (e) {
+  e.preventDefault();
+
+  validateName();
+  validateSurname();
+  validerEmail();
+  validateAdress();
+  validateCP();
+  validateCity();
+
+  const leformulaire = {
+    nom: document.querySelector("#votrenom").value,
+    prenom: document.querySelector("#prenom").value,
+    email: document.querySelector("#email").value,
+    adresse: document.querySelector("#adresse").value,
+    code_postal: document.querySelector("#cp").value,
+    ville: document.querySelector("#ville").value,
+  };
+
+  console.log(leformulaire);
+});
+
+function validateName() {
+  let inputName = document.querySelector("#votrenom");
+  let valueName = inputName.value;
+  let errorName = document.querySelector(".nom");
+  //trim permet de supprimer les espaces au extremités d'une chaine de caractères
+  if (!valueName.trim()) {
+    errorName.innerText = "Veuillez remplir ce champ";
+    errorName.style.display = "block";
+    inputName.value = ""; // '' permet de vider le champs entierement
+    return false;
+  }
+  if (valueName.length < 2) {
+    errorName.innerText = "Votre nom est trop court";
+    errorName.style.display = "block";
+    return false;
+  }
+  errorName.style.display = "none";
+  return true;
+}
+
+function validateSurname() {
+  let inputSurname = document.querySelector("#prenom");
+  let valueSurname = inputSurname.value;
+  let errorSurname = document.querySelector(".prenom");
+  //trim permet de supprimer les espaces au extremités d'une chaine de caractères
+  if (!valueSurname.trim()) {
+    errorSurname.innerText = "Veuillez remplir ce champ";
+    errorSurname.style.display = "block";
+    inputSurname.value = ""; // '' permet de vider le champs entierement
+    return false;
+  }
+  if (valueSurname.length < 2) {
+    errorSurname.innerText = "Votre prenom est trop court";
+    errorSurname.style.display = "block";
+    return false;
+  }
+}
+
+function validerEmail() {
+  let inputMail = document.getElementById("email");
+  let valueMail = inputMail.value;
+  let errorMail = document.querySelector(".email");
+  var regx = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+
+  if (!regx.test(inputMail.value)) {
+    errorMail.innerText = "Adresse email incorrecte";
+    errorMail.style.display = "block";
+    inputMail.value = ""; // '' permet de vider le champs entierement
+    inputMail.focus;
+    return false;
+  } else {
+    errorMail.style.display = "none";
+  }
+}
+
+function validateAdress() {
+  let inputAdress = document.querySelector("#adresse");
+  let valueAdress = inputAdress.value;
+  let errorAdress = document.querySelector(".adresse");
+  //trim permet de supprimer les espaces au extremités d'une chaine de caractères
+  if (!valueAdress.trim()) {
+    errorAdress.innerText = "Veuillez remplir ce champ";
+    errorAdress.style.display = "block";
+    inputAdress.value = ""; // '' permet de vider le champs entierement
+    return false;
+  }
+  if (valueAdress.length < 5) {
+    errorAdress.innerText = "Adresse erronée";
+    errorAdress.style.display = "block";
+    return false;
+  }
+  errorAdress.style.display = "none";
+  return true;
+}
+
+function validateCP() {
+  let inputCP = document.querySelector("#cp");
+  let valueCP = inputCP.value;
+  let errorCP = document.querySelector(".cp");
+  //trim permet de supprimer les espaces au extremités d'une chaine de caractères
+  if (!valueCP.trim()) {
+    errorCP.innerText = "Veuillez remplir ce champ";
+    errorCP.style.display = "block";
+    inputCP.value = ""; // '' permet de vider le champs entierement
+    return false;
+  }
+  if (valueCP.length < 5) {
+    errorCP.innerText = "Entrer un code postal correct";
+    errorCP.style.display = "block";
+    return false;
+  }
+  errorCP.style.display = "none";
+  return true;
+}
+
+function validateCity() {
+  let inputCity = document.querySelector("#ville");
+  let valueCity = inputCity.value;
+  let errorCity = document.querySelector(".ville");
+  //trim permet de supprimer les espaces au extremités d'une chaine de caractères
+  if (!valueCity.trim()) {
+    errorCity.innerText = "Veuillez remplir ce champ";
+    errorCity.style.display = "block";
+    inputCity.value = ""; // '' permet de vider le champs entierement
+    return false;
+  }
+  if (valueCity.length < 2) {
+    errorCity.innerText = "Veuillez recommencer";
+    errorCity.style.display = "block";
+    return false;
+  }
+  errorCity.style.display = "none";
+  return true;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 /*function afficherProduits(){
     let datas = localStorage.getItem('produits');
     if (datas){
@@ -21,272 +280,3 @@
     }
 }
 afficherProduits();*/
-
-let produitLocalStorage = JSON.parse(localStorage.getItem('produits'));
-console.log(produitLocalStorage);
-produits = produitLocalStorage;
-//selection de la classe ou je vais injecter mon html
-const positionsElement = document.querySelector('.tbody');
-//si le panier est vide : afficher le panier est vide
-let structureProduitPanier =  [];
-if (produits === null)
-{
-    const paniervide = `
-    <div class = "containerPanierVide">
-        <div>LE PANIER EST VIDE</div>
-        <a href="index.html">Retourner à la boutique</a>
-    </div>
-    `
-    let tableau = document.querySelector('.tableau');
-    tableau.innerHTML= paniervide;
-}else 
-{
-
-    // si le panier n'est pas vide , afficher les produits dans le localstorage
-    let nbproduit = 0;
-
-
-    for (j = 0; j < produits.length; j++){
-        
-        nbproduit += produits[j].quantity;
-        structureProduitPanier = structureProduitPanier +  `
-       
-            <tr>
-                <td class="divNomCouleur">${produitLocalStorage[j].name}</td>
-                <td class="quantity">${produitLocalStorage[j].quantity}</td>
-                <td class="coloris">${produitLocalStorage[j].selectedColor}</td>
-                <td class="prix">${produitLocalStorage[j].price}</td>
-                <td class="soustotal"></td>
-
-            </tr>
-            `;
-  
-}
-
-if (j === produitLocalStorage.length);{
-positionsElement.innerHTML = structureProduitPanier;
-    }
-    console.log('je suis ici ' + nbproduit);
-}
-
-//-------------------------------------------------TOTAL Prix produit----------------------------------------
-
-for (m = 0; m < produits.length; m++){
-    let quantité = produitLocalStorage[m].quantity;
-    let prixUnitaire = produitLocalStorage[m].price;
-
-    sousTotal = quantité * prixUnitaire;
-
-    console.log(sousTotal);
-
-}
-
-
-//-------------------------------------------------TOTAL PANIER----------------------------------------
-
-// Déclaration d'une variable pour y mettre les prix present dans le panier
-
-let prixTotalCalcul = [];
-
-// Ensuite on va chercher les prix dans le panier
-
-for (let k = 0; k < produitLocalStorage.length; k++){
-    let prixProduitPanier = produitLocalStorage[k].price;
-   
-
-    // Mettre à present les prix dans un tableau , donc dans la variable prixTotalCalcul
-
-    prixTotalCalcul.push(prixProduitPanier);
-    console.log(prixTotalCalcul);
-}
-
-// additonner le tout avec reduce
-
-const reducer = (acc, cur) => acc + cur;
-const prixTotal = prixTotalCalcul.reduce(reducer);
-console.log(prixTotal);
-
-// le html du prix total :
-
-const afficherPrixTotal = `<div class="prixTotal">Le prix total est : ${prixTotal}</div>`;
-//selection de la classe ou je vais injecter mon html
-const positionsElement2 = document.querySelector('.tfoot');
-positionsElement2.innerHTML = afficherPrixTotal;
-
-
-//-----------------------------------VIDER LE PANIER-------------------------------------------------
-
-
-let btn_viderPanier = document.querySelector('.paniervide');
-
-//suppression de la key produit du localstorage
-
-btn_viderPanier.addEventListener('click', (e)=>{
-    e.preventDefault();
-    localStorage.removeItem('produits');
-    alert('Le panier a été vidé');
-    window.location.href = "panier.html";
-
-});
-
-
-
-
-//-----------------------------------------FORMULAIRE-------------------------------------------------
-
-
-//selection du bouton
-
-let btn_envoyerleformulaire = document.querySelector('.bouton_commande');
-
-//adEvenetlistener
-
-btn_envoyerleformulaire.addEventListener('click',function(e){
-    e.preventDefault();
-
-validateName();
-validateSurname();
-validerEmail();
-validateAdress();
-validateCP();
-validateCity();
-
-
-    const leformulaire = {
-
-        nom : document.querySelector('#votrenom').value,
-        prenom : document.querySelector('#prenom').value,
-        email : document.querySelector('#email').value,
-        adresse : document.querySelector('#adresse').value,
-        code_postal : document.querySelector('#cp').value,
-        ville : document.querySelector('#ville').value
-    }
-    
-    console.log(leformulaire);
-
-} )
-
-function validateName(){
-    let inputName = document.querySelector('#votrenom');
-    let valueName = inputName.value;
-    let errorName = document.querySelector('.nom');
-    //trim permet de supprimer les espaces au extremités d'une chaine de caractères
-    if ( !valueName.trim()){
-        errorName.innerText = 'Veuillez remplir ce champ';
-        errorName.style.display = 'block';
-        inputName.value = '';// '' permet de vider le champs entierement
-        return false;
-    }
-    if (valueName.length < 2)
-    {
-        errorName.innerText = 'Votre nom est trop court';
-        errorName.style.display = 'block';
-        return false;
-    }
-    errorName.style.display = 'none';
-    return true;
-}
-
-function validateSurname(){
-    let inputSurname = document.querySelector('#prenom');
-    let valueSurname = inputSurname.value;
-    let errorSurname = document.querySelector('.prenom');
-    //trim permet de supprimer les espaces au extremités d'une chaine de caractères
-    if ( !valueSurname.trim()){
-        errorSurname.innerText = 'Veuillez remplir ce champ';
-        errorSurname.style.display = 'block';
-        inputSurname.value = '';// '' permet de vider le champs entierement
-        return false;
-    }
-    if (valueSurname.length < 2)
-    {
-        errorSurname.innerText = 'Votre prenom est trop court';
-        errorSurname.style.display = 'block';
-        return false;
-    }
-   
-
-}
-
-function validerEmail(){
-
-    let inputMail = document.getElementById('email');
-    let valueMail = inputMail.value;
-    let errorMail = document.querySelector('.email');
-    var regx = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/
-
-        if(!regx.test(inputMail.value)){
-            errorMail.innerText = 'Adresse email incorrecte';
-            errorMail.style.display = 'block';
-            inputMail.value = '';// '' permet de vider le champs entierement
-            inputMail.focus;
-            return false;
-        }
-        else{
-            errorMail.style.display = 'none';
-        }
-}
-
-
-function validateAdress(){
-    let inputAdress = document.querySelector('#adresse');
-    let valueAdress = inputAdress.value;
-    let errorAdress = document.querySelector('.adresse');
-    //trim permet de supprimer les espaces au extremités d'une chaine de caractères
-    if ( !valueAdress.trim()){
-        errorAdress.innerText = 'Veuillez remplir ce champ';
-        errorAdress.style.display = 'block';
-        inputAdress.value = '';// '' permet de vider le champs entierement
-        return false;
-    }
-    if (valueAdress.length < 5)
-    {
-        errorAdress.innerText = 'Adresse erronée';
-        errorAdress.style.display = 'block';
-        return false;
-    }
-    errorAdress.style.display = 'none';
-    return true;
-}
-
-function validateCP(){
-    let inputCP = document.querySelector('#cp');
-    let valueCP = inputCP.value;
-    let errorCP = document.querySelector('.cp');
-    //trim permet de supprimer les espaces au extremités d'une chaine de caractères
-    if ( !valueCP.trim()){
-        errorCP.innerText = 'Veuillez remplir ce champ';
-        errorCP.style.display = 'block';
-        inputCP.value = '';// '' permet de vider le champs entierement
-        return false;
-    }
-    if (valueCP.length < 5)
-    {
-        errorCP.innerText = 'Entrer un code postal correct';
-        errorCP.style.display = 'block';
-        return false;
-    }
-    errorCP.style.display = 'none';
-    return true;
-}
-
-function validateCity(){
-    let inputCity = document.querySelector('#ville');
-    let valueCity = inputCity.value;
-    let errorCity = document.querySelector('.ville');
-    //trim permet de supprimer les espaces au extremités d'une chaine de caractères
-    if ( !valueCity.trim()){
-        errorCity.innerText = 'Veuillez remplir ce champ';
-        errorCity.style.display = 'block';
-        inputCity.value = '';// '' permet de vider le champs entierement
-        return false;
-    }
-    if (valueCity.length < 2)
-    {
-        errorCity.innerText = 'Veuillez recommencer';
-        errorCity.style.display = 'block';
-        return false;
-    }
-    errorCity.style.display = 'none';
-    return true;
-}
--- script_page_produit.js
+++ script_page_produit.js
@@ -3,123 +3,115 @@
 const queryString_url_id = window.location.search;
 console.log(queryString_url_id);
 
-const IdPeluche = queryString_url_id.slice(4);//on isole l'id sans le préfixe
+const IdPeluche = queryString_url_id.slice(4); //on isole l'id sans le préfixe
 
-
- 
-async function test (){
-    const response = await fetch (`http://localhost:3000/api/teddies/${IdPeluche}`);
-    let data = await response.json();
-    maPeluche(data);
-    console.log(data)//on appelle la fonction de construction mapeluche et on y place data   
-}//on récupère les données de chaque produit avec le suffixe `${}`
+async function test() {
+  const response = await fetch(
+    `http://localhost:3000/api/teddies/${IdPeluche}`
+  );
+  let data = await response.json();
+  maPeluche(data);
+  console.log(data); //on appelle la fonction de construction mapeluche et on y place data
+} //on récupère les données de chaque produit avec le suffixe `${}`
 test();
 
-
-let body = document.querySelector('body');
-let lebody = document.createElement('div');
-lebody.classList.add('lebody');
-let retour = document.createElement('a');
+let body = document.querySelector("body");
+let lebody = document.createElement("div");
+lebody.classList.add("lebody");
+let retour = document.createElement("a");
 retour.href = "index.html";
-let button2 = document.createElement('button2');
-button2.innerText = 'Retour à la page précédente';
+let button2 = document.createElement("button2");
+button2.innerText = "Retour à la page précédente";
 body.appendChild(lebody);
 lebody.appendChild(retour);
 retour.appendChild(button2);
 
-
-
 function maPeluche(data) {
- 
-    let container = document.querySelector('.container');
-           
-            let img =  document.createElement('img');
-            img.src = data.imageUrl;
-            img.classList.add('image_produit');
-            img.alt = data.name;
-            container.appendChild(img);
-            let info_produit = document.createElement('div');
-            info_produit.classList.add('info_produit');
-            container.appendChild(info_produit);
-            let h1 = document.createElement('h1');
-            h1.innerText = data.name;
-            info_produit.appendChild(h1);
-            let price = document.createElement('p');
-            price.classList.add('price');
-            price.innerText = data.price + '€';
-            info_produit.appendChild(price);
-            let description = document.createElement('p');
-            description.classList.add('description');
-            description.innerText = data.description;
-            info_produit.appendChild(description);
-            let color = document.createElement('select');//CREATION DU MENU DEROULANT
-            color.classList.add('color');//LE MENU SAPEL COLOR
-            let alerte = document.createElement('small');//TEXT DERREUR
-            alerte.id = 'alerte'; //ON LUI DONNE UN ID ALERTE
-            info_produit.appendChild(color);
-            info_produit.appendChild(alerte);
-            let option = document.createElement('option'); // CREATION DU CHOIX PAR DEFAULT
-            option.innerText = 'choix des couleurs';
-            option.value = '';// cette option n'a pas de value donc quand elle est selecitonné elle est nulle
-            color.appendChild(option);
-            for (let i = 0; i < data.colors.length; i+=1)
-                {
-                    let info = data.colors[i]; //dans les données du produits on veut la clé colors
-                    let color1 = document.createElement('option');// ON CREE COLOR1 QUI CORRESPONDRA A CHAUQE COULEUR
-                    color1.classList.add('color1');
-                    color1.innerText = info;//COLOR1 CORRESPOND A INFO QUI LUI MEME CORRESPOND A DATA COLORS
-                    color1.value = info;//sa valeur c'est celle de info
-                    color.appendChild(color1);//on lui donne comme parent le menu deroulant 
-                };
-            color.addEventListener('change', function (e){ 
-                console.log(e);
-                let valeur = e.target.value; // le e correspond au type change, target correspond a lelement html donc ici c'est select, et value correspond à color
-                if (valeur){
-                    alerte.innerText = '';
-                }
+  let container = document.querySelector(".container");
 
-           })
-            let lien = document.createElement('a');
-            lien.href = "#";
-            let button = document.createElement('button');
-            button.innerText = 'Ajouter au panier';
-            lien.appendChild(button);
-            info_produit.appendChild(lien);
-            button.addEventListener('click', function (e){ 
-                e.preventDefault();
-                
-               let selectedColor = color.value;//variable pour la couleur choisi avec la valeur du menu deroulant
-                console.log(selectedColor);
-                if (!selectedColor){ // si différents de selectedcolor alors on affiche ce message
-                    alerte.innerText = "Veuillez sélectionner une couleur";
-                }
-                else 
-                {
-                    let produit = data;//produit equivant aux donnée du produit
-                    produit.selectedColor = selectedColor;
-                    let datas = localStorage.getItem('produits');
-                    //let panier = localStorage.getItem('produits') || [] pareil que ligne 77 sauf que l'on ne converti pas
-                    let panier = datas? JSON.parse(datas) : [];//: signifie if; data? signifie que si cette donnée est défini alors ca converti en json sinon ca renvoi un tableau vide
-                    let index = panier.findIndex(function (element){
-                        return element._id === produit._id && element.selectedColor === produit.selectedColor;
+  let img = document.createElement("img");
+  img.src = data.imageUrl;
+  img.classList.add("image_produit");
+  img.alt = data.name;
+  container.appendChild(img);
+  let info_produit = document.createElement("div");
+  info_produit.classList.add("info_produit");
+  container.appendChild(info_produit);
+  let h1 = document.createElement("h1");
+  h1.innerText = data.name;
+  info_produit.appendChild(h1);
+  let price = document.createElement("p");
+  price.classList.add("price");
+  price.innerText = data.price + "€";
+  info_produit.appendChild(price);
+  let description = document.createElement("p");
+  description.classList.add("description");
+  description.innerText = data.description;
+  info_produit.appendChild(description);
+  let color = document.createElement("select"); //CREATION DU MENU DEROULANT
+  color.classList.add("color"); //LE MENU SAPEL COLOR
+  let alerte = document.createElement("small"); //TEXT DERREUR
+  alerte.id = "alerte"; //ON LUI DONNE UN ID ALERTE
+  info_produit.appendChild(color);
+  info_produit.appendChild(alerte);
+  let option = document.createElement("option"); // CREATION DU CHOIX PAR DEFAULT
+  option.innerText = "choix des couleurs";
+  option.value = ""; // cette option n'a pas de value donc quand elle est selecitonné elle est nulle
+  color.appendChild(option);
+  for (let i = 0; i < data.colors.length; i += 1) {
+    let info = data.colors[i]; //dans les données du produits on veut la clé colors
+    let color1 = document.createElement("option"); // ON CREE COLOR1 QUI CORRESPONDRA A CHAUQE COULEUR
+    color1.classList.add("color1");
+    color1.innerText = info; //COLOR1 CORRESPOND A INFO QUI LUI MEME CORRESPOND A DATA COLORS
+    color1.value = info; //sa valeur c'est celle de info
+    color.appendChild(color1); //on lui donne comme parent le menu deroulant
+  }
+  color.addEventListener("change", function (e) {
+    console.log(e);
+    let valeur = e.target.value; // le e correspond au type change, target correspond a lelement html donc ici c'est select, et value correspond à color
+    if (valeur) {
+      alerte.innerText = "";
+    }
+  });
+  let lien = document.createElement("a");
+  lien.href = "#";
+  let button = document.createElement("button");
+  button.innerText = "Ajouter au panier";
+  lien.appendChild(button);
+  info_produit.appendChild(lien);
+  button.addEventListener("click", function (e) {
+    e.preventDefault();
 
-                })
-                    console.log(index);
-                    if (index >= 0){
-                        let exist = panier[index];
-                        exist.quantity = exist.quantity +1;
-                        panier[index] = exist; //pour remplacer le produit qui a été incrémenter, passer de 1 à 2 3 4..
-                    }
-                    else {
-                        produit.quantity = 1;
-                        panier.push(produit);//rentre les données du produits (data)dans le tableau "panier"
+    let selectedColor = color.value; //variable pour la couleur choisi avec la valeur du menu deroulant
+    console.log(selectedColor);
+    if (!selectedColor) {
+      // si différents de selectedcolor alors on affiche ce message
+      alerte.innerText = "Veuillez sélectionner une couleur";
+    } else {
+      let produit = data; //produit equivant aux donnée du produit
+      produit.selectedColor = selectedColor;
+      let datas = localStorage.getItem("produits");
+      //let panier = localStorage.getItem('produits') || [] pareil que ligne 77 sauf que l'on ne converti pas
+      let panier = datas ? JSON.parse(datas) : []; //: signifie if; data? signifie que si cette donnée est défini alors ca converti en json sinon ca renvoi un tableau vide
+      let index = panier.findIndex(function (element) {
+        return (
+          element._id === produit._id &&
+          element.selectedColor === produit.selectedColor
+        );
+      });
+      console.log(index);
+      if (index >= 0) {
+        let exist = panier[index];
+        exist.quantity = exist.quantity + 1;
+        panier[index] = exist; //pour remplacer le produit qui a été incrémenter, passer de 1 à 2 3 4..
+      } else {
+        produit.quantity = 1;
+        panier.push(produit); //rentre les données du produits (data)dans le tableau "panier"
+      }
 
-                    }
-                    
-                    localStorage.setItem('produits', JSON.stringify(panier))
-                }   
-                
-                window.location.href = 'panier.html';
-            })
-};
+      localStorage.setItem("produits", JSON.stringify(panier));
+    }
 
+    window.location.href = "panier.html";
+  });
+}
